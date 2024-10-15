c74ed6974c61dffb534cd2c4b5c52e1650b3bcae
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:14:21 -0400

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,32 @@
+
+public class Employee {
+ 
+ 
+private double salary = 40000.00;
+ 
+ 
+private int vacationDays = 10;
+ 
+ 
+private String vacationForm = "greengreengreengreen";
+ 
+ 
+public double getSalary() {
+ 
+return salary;
+ 
+ 
+}
+ 
+public int getVacationDays() {
+ 
+return vacationDays;
+ 
+}
+ 
+ 
+public String getVacationForm() {
+ 
+return vacationForm;
+ 
+}

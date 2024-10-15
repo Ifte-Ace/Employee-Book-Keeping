fd4e8f35442a6725c9a59d87b2f878832486c47c
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:12:49 -0400

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+ 
+public static void main(String[] args){
+ 
+StanfordLawyer s = new StanfordLawyer();
+ 
+System.out.println(s.getSalary());
+ 
+System.out.println(s.getVacationDays());
+ 
+System.out.println(s.getVacationForm());
+ 

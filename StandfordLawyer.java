5dce439e3bdeeedae45c1531efb68ad3e0a480c4
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:13:16 -0400

    Create StandfordLawyer.java

---
+++ StandfordLawyer.java
@@ -0,0 +1,20 @@
+
+public class StanfordLawyer extends Lawyer {
+ 
+public double getSalary() {
+ 
+return super.getSalary() * 1.2;
+ 
+}
+ 
+ 
+public int getVacationDays() {
+ 
+return super.getVacationDays() + 3;
+ 
+}
+ 
+public String getVacationForm() {
+ 
+return "pinkpinkpinkpink";
+ 

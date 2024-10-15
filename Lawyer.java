1773d55da1bf6bd39875eec5b74efe1e3c0aa0a1
Ifte <156860282+Ifte-Ace@users.noreply.github.com>
2024-10-15 15:13:56 -0400

    Create Lawyer.java

---
+++ Lawyer.java
@@ -0,0 +1,22 @@
+
+public class Lawyer extends Employee{
+ 
+ 
+public double getSalary() {
+ 
+return super.getSalary();
+ 
+ 
+}
+ 
+public int getVacationDays() {
+ 
+return super.getVacationDays();
+ 
+}
+ 
+ 
+public String getVacationForm() {
+ 
+return super.getVacationForm();
+ 
